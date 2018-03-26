483db7bfdbc896a8ee237a139f7f226b416fdd13
Jiajing Mao <jiajing.mao@cern.ch>
2018-03-26 19:22:15 +0200

    update renamePDF.py to fix bugs

--- HggRazorLeptons/FitMgg/AIC_output/renamePDF.py
+++ HggRazorLeptons/FitMgg/AIC_output/renamePDF.py
@@ -6,9 +6,10 @@ import subprocess, time, sys, os, glob
 if __name__ == "__main__":
 	directory = sys.argv[1]
 	original_file_names = glob.glob(directory+"/*.pdf")
-	for index in range(len(original_file_names)):		
-		new_name =  original_file_names[index].replace("_0.","_0d")
-		new_name_2 =  new_name.replace("_10.","_10d")
-		os.system("mv "+original_file_names[index]+" "+new_name_2)
+	for index in range(len(original_file_names)):
+                if "_0.pdf" not in original_file_names[index]:
+		        new_name =  original_file_names[index].replace("_0.","_0d")
+		        new_name_2 =  new_name.replace("_10.","_10d")
+		        os.system("mv "+original_file_names[index]+" "+new_name_2)
 
 		
